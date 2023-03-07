3cd9749fd15cc109950580058debd146382f48fd
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-03-07 11:39:54 +0000

    w

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,10 @@ use App\Http\Controllers\TaskController;
 */
 //tasukukannri
 Route::get('/task/list', [TaskController::class, 'list']);
-
+Route::get('/task/list', [TaskController::class, 'list']);
 
 //test
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
+//form in
+Route::get('/test', [TestController::class, 'index']);
