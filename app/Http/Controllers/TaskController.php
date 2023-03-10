cb46471d9cdf75e0eef0189f262acdfad611c1d0
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-03-10 07:08:58 +0000

    wrote

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
@@ -15,4 +18,35 @@ class TaskController extends Controller
      {
          return view('task.list');
      }
+     
+     /**
+      * タスクの新規登録
+      */
+    public function register(TaskRegisterPostRequest $request)
+    {
+        //validate済みのデータの取得
+        $datum = $request->validated();
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+        
+        // user_idの追加
+        $datum['user_id'] = Auth::id();
+        
+        // テーブルへのINSERT
+        try {
+        $r = TaskModel::create($datum);
+        
+    } catch(\Throwable $e) {
+        // XXX 本当はログ等に各処理をする。今回は一端出力するだけ
+        echo $e->getMessage();
+        exit;
+    }
+    
+    // タスク塘路k成功
+    $request->session()->flash('front.task_register_success', true);
+    //
+    return redirect('/task/list');
+    }
 }
