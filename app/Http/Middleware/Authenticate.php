12d38b3b14833640aa524978c1f13aae26d05573
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-03-17 05:40:36 +0000

    管理画面の認証と認可を書いた

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,11 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            if (0 === strncasecmp($request->path(), 'admin/', strlen('admin/'))) {
+                 return route('admin.index');
+        } else {
             return route('front.index');
         }
     }
+  }
 }
